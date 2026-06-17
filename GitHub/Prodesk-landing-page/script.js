5a99095078577183d9360a2ff9d5413b813db586
Shaili-Dixit <dixitshaili5@gmail.com>
2026-06-17 15:00:38 +0530

    prodesk web

---
+++ GitHub/Prodesk-landing-page/script.js
@@ -0,0 +1,15 @@
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-theme");
+
+    if(document.body.classList.contains("dark-theme")){
+        localStorage.setItem("theme","dark");
+    } else {
+        localStorage.setItem("theme","light");
+    }
+}
+
+window.onload = function(){
+    if(localStorage.getItem("theme") === "dark"){
+        document.body.classList.add("dark-theme");
+    }
+}
